f48046667152c0eb7ac9407ebd78e2bd66e52765
unyime1 <lordunyime@gmail.com>
2023-12-24 09:38:23 +0100

    feat:define basic application window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod window;
+
+use adw::Application;
+use gtk::{gdk::Display, gio, glib, prelude::*, CssProvider};
+use window::Window;
+
+const APP_ID: &str = "org.etim.querry";
+
+fn main() -> glib::ExitCode {
+    gio::resources_register_include!("querry.gresource").expect("Failed to register resources.");
+
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // // Connect to signals
+    app.connect_startup(|_| load_css());
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a new custom window and present it
+    let window = Window::new(app);
+    window.present();
+}
+
+fn load_css() {
+    // Load the CSS file and add it to the provider
+    let provider = CssProvider::new();
+    provider.load_from_resource("/org/etim/querry/style.css");
+
+    // Add the provider to the default screen
+    gtk::style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
