5eac10c04f1b1a2dea79c75369bcf894fd588580
unyime1 <lordunyime@gmail.com>
2023-12-27 17:20:54 +0100

    feat:connect sidebar click listener

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -37,13 +37,21 @@ impl Window {
     pub fn connect_rest_clicked(&self) {
         let stack_sidebar = self.imp().stack_sidebar.clone();
 
-        // Rest button.
         let rest_box = self.imp().rest_box.clone();
+        let realtime_box = self.imp().realtime_box.clone();
+        let settings_box = self.imp().settings_box.clone();
+        let rest_box_clone = rest_box.clone();
+
         let gesture_rest = gtk::GestureClick::new();
         gesture_rest.connect_released(move |gesture, _, _, _| {
             gesture.set_state(gtk::EventSequenceState::Claimed);
             let stack = &stack_sidebar.stack().expect("No stack");
+
+            // Manage active classes.
             stack.set_visible_child_name("rest_page");
+            rest_box_clone.add_css_class("active-sidebar");
+            realtime_box.remove_css_class("active-sidebar");
+            settings_box.remove_css_class("active-sidebar");
         });
         rest_box.add_controller(gesture_rest);
     }
@@ -51,14 +59,22 @@ impl Window {
     /// Connect clicked signal to realtime button.
     pub fn connect_realtime_clicked(&self) {
         let stack_sidebar = self.imp().stack_sidebar.clone();
-        // Realtime button.
 
+        let rest_box = self.imp().rest_box.clone();
         let realtime_box = self.imp().realtime_box.clone();
+        let settings_box = self.imp().settings_box.clone();
+        let realtime_box_clone = realtime_box.clone();
+
         let gesture_realtime = gtk::GestureClick::new();
         gesture_realtime.connect_released(move |gesture_real, _, _, _| {
             gesture_real.set_state(gtk::EventSequenceState::Claimed);
             let stack = &stack_sidebar.stack().expect("No stack");
+
+            // Manage active classes.
             stack.set_visible_child_name("realtime_page");
+            realtime_box_clone.add_css_class("active-sidebar");
+            settings_box.remove_css_class("active-sidebar");
+            rest_box.remove_css_class("active-sidebar");
         });
         realtime_box.add_controller(gesture_realtime);
     }
@@ -66,13 +82,22 @@ impl Window {
     /// Connect clicked signal to settings button.
     pub fn connect_settings_clicked(&self) {
         let stack_sidebar = self.imp().stack_sidebar.clone();
-        // Setting button.
+
+        let rest_box = self.imp().rest_box.clone();
+        let realtime_box = self.imp().realtime_box.clone();
         let settings_box = self.imp().settings_box.clone();
+        let settings_box_clone = settings_box.clone();
+
         let gesture_settings = gtk::GestureClick::new();
         gesture_settings.connect_released(move |gesture_s, _, _, _| {
             gesture_s.set_state(gtk::EventSequenceState::Claimed);
             let stack = &stack_sidebar.stack().expect("No stack");
+
+            // Manage active classes.
             stack.set_visible_child_name("settings_page");
+            settings_box_clone.add_css_class("active-sidebar");
+            rest_box.remove_css_class("active-sidebar");
+            realtime_box.remove_css_class("active-sidebar");
         });
         settings_box.add_controller(gesture_settings);
     }
