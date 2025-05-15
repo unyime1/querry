43b71359ab82ecb89964080d1db262f548a21f51
Unyime Etim <32114147+etim66@users.noreply.github.com>
2025-05-15 13:28:19 +0100

    Add Build CI (#38)
    * fix:ci fixes checkpoint 1
    
    * feat:complete ci fixes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::rc::Rc;
-
-use database::migrate_database;
 use slint::{ComponentHandle, PlatformError};
-
-mod database;
-mod utils;
 
 use lib::{
     callbacks::{
@@ -18,27 +12,26 @@ use lib::{
         },
         images::process_get_images,
     },
-    database::get_database,
-    AppConfig, AppWindow,
+    database::{get_database, migrate_database},
+    AppWindow,
 };
 
 fn main() -> Result<(), PlatformError> {
     let db = get_database().expect("Could not connect to DB");
-    let shared_db = Rc::new(db);
 
-    migrate_database(shared_db.clone()).expect("Could not apply migrations");
+    migrate_database(db.clone()).expect("Could not apply migrations");
 
     let app = AppWindow::new()?;
 
-    check_startup_page(shared_db.clone(), &app).unwrap();
-    load_collections(shared_db.clone(), &app).unwrap();
+    check_startup_page(db.clone(), &app).unwrap();
+    load_collections(db.clone(), &app).unwrap();
     process_page_change(&app).unwrap();
-    process_get_collections(shared_db.clone(), &app).unwrap();
-    process_create_collection(shared_db.clone(), &app).unwrap();
+    process_get_collections(db.clone(), &app).unwrap();
+    process_create_collection(db.clone(), &app).unwrap();
     process_get_images(&app).unwrap();
-    process_update_collection(shared_db.clone(), &app).unwrap();
-    process_remove_collection(shared_db.clone(), &app).unwrap();
-    process_search_collections(shared_db.clone(), &app).unwrap();
+    process_update_collection(db.clone(), &app).unwrap();
+    process_remove_collection(db.clone(), &app).unwrap();
+    process_search_collections(db.clone(), &app).unwrap();
 
     app.run()?;
     Ok(())
